2e3dbe385ee7b16ecd2f11abfb3f80c5ec3b3d8c
vietbuii <vietbh.26@gmail.com>
2024-12-02 22:43:05 +0700

    first commit

---
+++ src/main/java/com/javatechie/jenkinscicdpipeline/JenkinsCicdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.jenkinscicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/jenkinscicdpipeline/JenkinsCicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.jenkinscicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
